0bdfa6ab51904f2c421aa74a72730ce01e9a93ad
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-11-06 17:14:10 -0500

    adds pug, import path module and create three routes for three seperate pugs

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const express = require('express')
+const path = require('path')
 
-const app = express();
-
+//port
 const port = process.env.PORT || 3000
 
 
+//init app
+const app = express();
 
+//pug view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug');
+
+//home route
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index')
 })
 
+//about route
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+//contact route
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+
+
+
+//start server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-
-
