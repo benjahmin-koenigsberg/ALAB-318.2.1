6d64bfe03d732fcf52a803c22d1f3e7da16fd4c3
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-11-07 18:41:32 -0500

    adds json data i seperate file and endpoint to return json data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const users = require('./users.js')
+
 
 //port
 const port = process.env.PORT || 3000
@@ -31,13 +33,21 @@ app.post('/contact', (req, res) => {
     res.send('Post request successfully recieved')
 })
 
+
+
 // app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname)))
 
 
-
+//image
 app.get('/image', ( req, res) => {
     res.sendFile(path.join(__dirname, './public/image.jpeg'))
+})
+
+
+//users from json placeholser
+app.get('/api/users', (req, res) => {
+    res.json(users)
 })
 
 
